4bde19e6e15fa3cd011ee09b95e3efb1e5121641
Vladimir <krechetvova@gmail.com>
2025-02-02 18:02:41 +0700

    checking_value

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,3 +1,5 @@
+import json
+
 from requests import Response
 from utils.checking import Checking
 from  utils.api import Google_maps_api
@@ -13,24 +15,33 @@ class Test_create_place():
         check_post = result_post.json()   #Обращаемся к result_post, чтобы вернулся ответ в формате json
         place_id = check_post.get("place_id")   #Просим check_post вернуть значение переменной place_id
         Checking.check_status_code(result_post, 200)
+        Checking.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
 
         print("Метод GET POST")
         result_get: Response = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website', 'language'])
 
         print("Метод PUT")
         result_put: Response = Google_maps_api.put_new_place(place_id)
         Checking.check_status_code(result_put, 200)
+        Checking.check_json_token(result_put, ['msg'])
 
         print("Метод GET PUT")
         result_get: Response = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get,['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website','language'])
 
         print("Метод DELETE")
         result_delete: Response = Google_maps_api.delete_new_place(place_id)
         Checking.check_status_code(result_delete, 200)
+        Checking.check_json_token(result_delete, ['status'])
 
         print("Метод GET DELETE")
         result_get: Response = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 404)
+        Checking.check_json_token(result_get, ['msg'])
+        # token = json.loads(result_get.text)
+        # print(list(token))
 
+        print("Тестирование создание, изменения и удаления новой локации прошло успешно!!!")
